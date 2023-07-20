8249bc42a9aa62d0fa615a37f510b13f77bef4c5
andrealicheri <andrealicheripakunza@gmail.com>
2023-07-20 15:32:00 +0200

    Alpinejs Persist + JSConfig shortcuts

--- src/ts/alpine.ts
+++ src/ts/alpine.ts
@@ -1,4 +1,7 @@
-// Import Alpine plugins BEFORE this comment
 import Alpine from "alpinejs"
+import persist from '@alpinejs/persist'
+
+Alpine.plugins(persist)
+// Load Alpine plugins BEFORE this comment
 window.Alpine = Alpine
 Alpine.start()
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="vite/client" />
 declare module 'alpinejs'
+declare module '@alpinejs/persist'
 declare var Alpine: any;
